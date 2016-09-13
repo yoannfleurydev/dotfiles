44a9824be45a4eba67706d1f87c8d2b6d669f2d9
Yoann Fleury <yoann.fleury@yahoo.com>
2016-09-13 13:30:07 +0200

    feat(git) : add some git aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git pull origin master
+
 if [ -e /usr/bin/git ]; then
     mkdir -p $HOME/etc/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/etc/zsh-syntax-highlighting
