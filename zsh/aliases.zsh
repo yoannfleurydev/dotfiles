137e3a91c48932974a57495bcbe658639aa1d95d
Yoann Fleury <yoann.fleury@yahoo.com>
2016-09-21 17:09:57 +0200

    feat(zsh) : put aliases into a different file

---
+++ zsh/aliases.zsh
@@ -0,0 +1,7 @@
+alias ez="$EDITOR ~/.zshrc"
+alias ev="$EDITOR ~/.vim/vimrc"
+
+alias ls="ls --color=auto"
+alias ll="ls -alh --color=auto"
+
+alias upgrade="apt update && apt upgrade && apt clean && apt autoremove"
