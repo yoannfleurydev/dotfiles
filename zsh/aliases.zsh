5e934b2da48699298c60682d4d4c66c95c4f8f22
Yoann Fleury <yoann.fleury@yahoo.com>
2016-11-17 12:45:19 +0100

    feat(zsh): add upgrade-all alias to upgrade npm

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,4 @@ alias ls="ls --color=auto"
 alias ll="ls -alh --color=auto"
 
 alias upgrade="apt update && apt upgrade && apt clean && apt autoremove"
+alias upgrade-all="upgrade && sudo n latest && sudo npm update -g"
