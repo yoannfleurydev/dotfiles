312a32c86fd54f3490c8e63affb28f05f6f14fb7
Yoann Fleury <yoann.fleury@yahoo.com>
2017-01-14 18:17:48 +0100

    feat(git): add commit template

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,15 @@
 
 git pull origin master
 
-if [ -e /usr/bin/git ]; then
+if [ $(hash git) -z ]; then
+  if [ "$HOME/etc/zsh-syntax-highlighting" -d ]; then
     mkdir -p $HOME/etc/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/etc/zsh-syntax-highlighting
+  else
+    echo "Plugin ZSH Syntax Highlighting already exists"
+  fi    
 else
-    echo "You need git" >&2
-    exit 1
+  echo "You need git" >&2
+  exit 1
 fi
+
