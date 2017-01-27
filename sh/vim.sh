19019bf7d257fe311cc13ea7a52682848c004538
Yoann Fleury <yoann.fleury@yahoo.com>
2017-01-27 11:21:25 +0100

    feat(vim): add installation script

--- sh/vim.sh
+++ sh/vim.sh
@@ -6,3 +6,14 @@ if [ $? -ne 0 ]; then
     apt update
     apt install vim -y
 fi
+
+hash git 2> /dev/null
+if [ $? -eq 0 ]; then
+    echo "Installing Vundle"
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+echo "Link the vimrc file"
+ln -s /home/yfleury/etc/dotfiles/vim/vimrc /home/yfleury/.vim/vimrc
+
+vim +PluginInstall +qall
