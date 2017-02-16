d11a9fe3a67aa791ca85ba701d6da850e187cbf1
Yoann Fleury <yoann.fleury@yahoo.com>
2017-02-16 22:05:20 +0100

    feat(brew,gem): add more packages to install

--- sh/brew.sh
+++ sh/brew.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+ln -s $HOME/etc/dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+
 brew install git-cal
 brew install tig
 brew install the_silver_searcher
+brew install ranger
---
+++ sh/gem.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo gem install lolcat
