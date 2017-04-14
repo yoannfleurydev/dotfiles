d5cb549424e974f2f1577953c6073a9951a55939
Yoann Fleury <yoann.fleury@smartpanda.com>
2017-04-14 15:22:50 +0200

    :heavy_plus_sign:(zsh) : add new suggestion plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,12 @@ sudo gem install terminal-emojify
 # ZSH Plugin
 mkdir -p $HOME/etc/zsh-syntax-highlighting
 mkdir -p $HOME/etc/zsh-completions
+mkdir -p $HOME/etc/zsh-autosuggestions
 mkdir -p $HOME/etc/k
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/etc/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-completions.git $HOME/etc/zsh-completions
+git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/etc/zsh-autosuggestions
 git clone git@github.com:supercrabtree/k.git $HOME/etc/k
 
 rm -f ~/.zcompdump; compinit
