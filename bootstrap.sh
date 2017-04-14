be707410298e4fd94c12e5a0a5c7a1fbc4425f62
Yoann Fleury <yoann.fleury@smartpanda.com>
2017-04-14 10:17:10 +0200

    :sparkles: : new zsh completion plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,14 @@ sudo gem install terminal-emojify
 
 # ZSH Plugin
 mkdir -p $HOME/etc/zsh-syntax-highlighting
+mkdir -p $HOME/etc/zsh-completions
 mkdir -p $HOME/etc/k
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/etc/zsh-syntax-highlighting
+git clone git://github.com/zsh-users/zsh-completions.git $HOME/etc/zsh-completions
 git clone git@github.com:supercrabtree/k.git $HOME/etc/k
+
+rm -f ~/.zcompdump; compinit
 
 # Brew installation
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
