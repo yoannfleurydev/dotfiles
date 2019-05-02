e25e86e7b070b8b4d7a11a35f6bce6e9dd7305bf
Yoann Fleury <yoann.fleury@yahoo.com>
2019-05-02 08:20:34 +0200

    feat(fish): add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,33 @@
+# -----------------------
+# fish configuration file
+# -----------------------
+
+# The greeting function will print on new shell
+function fish_greeting
+    # Empty function so no greeting will be prompted
+end
+
+eval (direnv hook fish)
+
+set HOME_BIN $HOME/bin
+set -x TERMINAL gnome-terminal
+set -x EDITOR vim
+set -x BROWSER google-chrome-stable
+set -x VISUAL vim
+set -x LC_ALL "en_US.UTF-8"
+
+set -x JAVA_HOME $HOME_BIN/java/jdk1.8.0_212
+set -x GRADLE_HOME $HOME_BIN/gradle
+set -x GRADLE_USER_HOME $HOME/.gradle
+set -x ANDROID_HOME $HOME_BIN/android/sdk
+set -x MVN_HOME $HOME_BIN/maven
+set -x CATALINA_HOME $HOME_BIN/tomcat
+set -x KAFKA_HOME $HOME_BIN/kafka
+set -x FLUTTER_HOME $HOME_BIN/flutter
+set -x XDG_CONFIG_HOME $HOME/.config
+
+# PATH {{{
+set -x PATH $HOME/.cargo/bin $MVN_HOME/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $GRADLE_HOME/bin $HOME/.local/bin $FLUTTER_HOME/bin $JAVA_HOME/bin $HOME/bin $HOME/.npm-global/bin $PATH
+#}}}
+
+source ~/.config/fish/aliases.fish
