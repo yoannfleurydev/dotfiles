a3ccb138cdce485e8d2263e77770c2e5ceaad9f4
Yoann Fleury <yoann.fleury@yahoo.com>
2019-07-27 12:59:48 +0200

    feat(fish): add google cloud sdk path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -25,9 +25,10 @@ set -x CATALINA_HOME $HOME_BIN/tomcat
 set -x KAFKA_HOME $HOME_BIN/kafka
 set -x FLUTTER_HOME $HOME_BIN/flutter
 set -x XDG_CONFIG_HOME $HOME/.config
+set -x GOOGLE_CLOUD_HOME $HOME_BIN/google-cloud-sdk
 
 # PATH {{{
-set -x PATH $HOME/.cargo/bin $MVN_HOME/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $GRADLE_HOME/bin $HOME/.local/bin $FLUTTER_HOME/bin $JAVA_HOME/bin $HOME/bin $HOME/.npm-global/bin $PATH
+set -x PATH $GOOGLE_CLOUD_HOME/bin $HOME/.cargo/bin $MVN_HOME/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools $GRADLE_HOME/bin $HOME/.local/bin $FLUTTER_HOME/bin $JAVA_HOME/bin $HOME/bin $HOME/.npm-global/bin $PATH
 #}}}
 
 source ~/etc/dotfiles/config/fish/aliases.fish
@@ -37,3 +38,6 @@ if test -e ~/etc/dotfiles/config/fish/work.fish
     source ~/etc/dotfiles/config/fish/work.fish
 end
 
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/home/yfleury/bin/google-cloud-sdk/path.fish.inc' ]; . '/home/yfleury/bin/google-cloud-sdk/path.fish.inc'; end
