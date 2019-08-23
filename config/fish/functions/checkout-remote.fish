88fd8bd0190688c6a316820c5eb94d38e1f5d387
Yoann Fleury <yoann.fleury@yahoo.com>
2019-08-23 09:39:50 +0200

    fix(fish): checkout-remote incorrect sed

--- config/fish/functions/checkout-remote.fish
+++ config/fish/functions/checkout-remote.fish
@@ -1,4 +1,4 @@
-# Defined in /tmp/fish.wWmc01/checkout-remote.fish @ line 2
+# Defined in /home/yfleury/.config/fish/functions/checkout-remote.fish @ line 2
 function checkout-remote
 	set branches (git branch --all | grep -v HEAD)
     set branch (printf '%s\n' $branches | fzf +m)
